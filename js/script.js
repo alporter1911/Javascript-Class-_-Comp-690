5e1554e2b60ded3dcd8c5f66b9f91ef165993265
A.L.Porter <adrian.porter1911@gmail.com>
2025-02-23 08:04:43 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,176 @@
+// // STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFuntion;
+
+
+// // STEP 2
+// // Example of a Numeric Literal
+// let numbericLiteral = 21;
+// // Example of a String Literal 
+// let stringLiteral = "Hello, Professor";
+// // Example of a Boolean Literal 
+// let booleanLiteral = true;
+// // Example of a Null Literal 
+// let nullLiteral = null;
+
+
+// // STEP 3
+// let a = 20;
+// let b = 10;
+// let result = (a * 2) + (b / 2) + 3;// complex expression
+// console.log(result); // result = 48
+
+// let firstName = "Adrian";
+// let lastName = "Porter";
+// let fullName = firstName + " " + lastName + " is a cybersecurity expert.";
+// console.log(fullName); // Adrian Porter is a cybersecurity expert.
+
+
+// // STEP 4
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let intZipCode;
+// let intYourAge;
+// let strReferralSource;
+// let boolMayweContactYou;
+
+
+// //STEP 5
+// let strFirstName;
+// strFirstName = "Adrian";
+
+// let intYourAge = 43;
+
+// let strCity = "Coronado", strState = "California", intZipCode = 91235;
+
+
+// //STEP 6
+// let myVariable = 20 + "Bananas";
+// console.log(myVariable);
+
+
+// //STEP 7
+// let myBooleanString = true + " is the truth";
+// console.log(myBooleanString);
+
+// let myNumberBoolean = 5 + true;
+// console.log(myNumberBoolean); // Output: 6
+
+
+// //STEP 8
+// //Not valid: using both single and double quotations incorrectly
+// //Correction
+// let someString = `Who once said, "Only two things are infinite, the universe and human stupidity, and I'm sure about the former."`;
+
+
+
+// //STEP 9
+// let myNullVariable = null;
+// console.log(myBooleanString);
+
+// let myUndefinedVariable;
+// console.log(myUndefinedVariable);
+
+
+// //STEP 10
+// console.log(typeof "Hello, sir.");
+// console.log(typeof 42);
+// console.log(typeof true);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+
+// //STEP 11
+// let firstName = "Adrian";
+// let lastName = "Porter";
+// let course = "Javascript";
+
+// alert("Hello " + firstName + " " + lastName + ", welcome to the " + course + " class!");
+
+
+// //STEP 12
+// let name = "Adrian";
+// let lastname = "Porter";
+// let course = "Javascript";
+
+// alert("Hello " + name + " " + lastName + ", welcome to the " + course + " class!");
+
+
+// //STEP 13
+// let myVariable1 = "Javascript";
+
+// //STEP 14
+// let name1 = "Adrian";
+// let lastName1 = "Porter";
+// let course1 = "Javascript";
+
+// alert("Hello " + name1 + " " + lastName1 + ".\nWelcome to the " + course1 + " class!");
+
+
+// //STEP 15
+// let namePrompt = prompt("What is your name?");
+// let lastName2 = prompt("What is your last name?");
+// let course2 = "Javascript";
+
+// alert("Hello " + namePrompt + " " + lastName2 + ".\nWelcome to the " + course2 + " class!");
+
+
+// //STEP 16
+// let namePrompt1 = prompt("What is your name?");
+// let lastName3 = prompt("What is your last name?");
+// let course3 = prompt("What class are you taking?");
+
+// alert("Hello " + namePrompt1 + " " + lastName3 + ".\nWelcome to the " + course3 + " class!");
+
+
+// //STEP 17
+// let x =10;
+// let y = 20;
+// let sum = x + y;
+
+// console.log(sum);
+
+
+// //STEP 18
+// let x = 20;
+// x += 20;
+
+// console.log(x);
+
+
+// //STEP 19
+// let x = 20;
+// x *= 5;
+
+// console.log(x);
+
+
+// //STEP 20
+// let x = 20 % 3;
+// x /= 1;
+
+// console.log(x);
+
+
+// //STEP 21
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// let answer = (a < b) && (b < c) || (c === 3);
+// console.log(answer);
+
+
+// //STEP 22
+// let a = 100;
+// let b = 20;
+// let c = 5;
+
+// let answer1 = (a <= b) || (b >= c) && (c !== 5);
+// console.log(answer1);
